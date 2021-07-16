c25af763f9943437de1fc79dbc9ec16126c0da77
Gabor Kovacs <gkovacs@uw.edu>
2021-07-15 17:31:24 -0700

    Add example csv and notebook.

--- python/calculator.py
+++ python/calculator.py
@@ -1,6 +1,21 @@
 from astropy.tables import Table
 
 
-class ShareHistory:
+class AssetHistory:
     def ingestTransactions(self, T):
+        """Creates a 1-to-1 buy-sell transaction pairing, FIFO order
+
+        Parameters
+        ----------
+        T : `astropy.table.Table`
+        'assetId' : unique id
+        'tranType' : 'sell', 'buy'
+        'unitPrice' : float
+        'quantity' : number of units bought/sold
+        """
         pass
+
+
+class ValueTable:
+    """Stores known valuation points then interpolates if necessary for retrieval."""
+    pass
